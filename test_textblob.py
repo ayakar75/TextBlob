233bc465071eb685ae5b8ca348c9a5b8a86bd2fd
Abdurrahman YAKAR <ryakar75@gmail.com>
2024-12-31 19:14:06 +0300

    feat: second commit

--- test_textblob.py
+++ test_textblob.py
@@ -1,5 +1,14 @@
-from example import TextBlob
+from textblob import TextBlob
 
-text = "Today is a beautiful day. Tomorrow looks like bad weather."
+text = "Today is a so beautiful day. Tomorrow looks like better than today."
 
 blob = TextBlob(text)
+
+# Analiz sonuçları
+print("Sentences:", blob.sentences)  # Cümleler
+print("Words:", blob.words)          # Kelimeler
+print("Sentiment:", blob.sentiment)  # Duygu analizi
+print("Tags:", blob.tags)
+
+for word, pos in blob.tags:
+    print(f"{word}: {pos}")
